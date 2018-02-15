27ac368b66c6bb643fc26117913fd7bf171593f8
Antoine <36314396+antoinepetty@users.noreply.github.com>
2018-02-15 10:44:30 +0000

    Now works

--- EthRecieve.py
+++ EthRecieve.py
@@ -1,10 +1,22 @@
 import socket
-UDP_IP = "0.0.0.0"
+
+UDP_IP = "169.254.89.200"
 UDP_PORT = 8888
+#MESSAGE = b'2000'
+MESSAGE = "Hello"
+
+
+print ("UDP target IP:", UDP_IP)
+print ("UDP target port:", UDP_PORT)
+print ("message:", MESSAGE)
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
+#sock.bind((UDP_IP, UDP_PORT))
+#sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+sock.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP, UDP_PORT))
+
+#sock.bind((UDP_IP, UDP_PORT))
 
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
