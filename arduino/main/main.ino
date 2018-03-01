5017b2f0aa44e13d3c0785c55783483b308b62a8
Antoine <36314396+antoinepetty@users.noreply.github.com>
2018-03-01 11:29:31 +0000

    Surface-pi communication

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,6 +1,7 @@
 #include <WString.h>
 //int incomingByte = 0;   // for incoming serial data
-char incomingString[] = "";
+//char incomingString[] = "";
+String incomingString = "";
 
 void setup() {
   Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
@@ -14,14 +15,17 @@ void loop() {
   if (Serial.available() > 0) {
     // read the incoming byte:
     //incomingString = Serial.readString().toCharArray();
-    Serial.readBytes(incomingString,4);
+    incomingString = Serial.readString();
 
-    if(incomingString=="test"){
-      digitalWrite(LED_BUILTIN, HIGH);
-      delay(1000);
-      digitalWrite(LED_BUILTIN, LOW);
-    }
+//    if(incomingString=="test"){
+//      digitalWrite(LED_BUILTIN, HIGH);
+//      delay(1000);
+//      digitalWrite(LED_BUILTIN, LOW);
+//    }
+
     // say what you got:
+    delay(5); //Delay to allow some time for the arduino to actually read the data
+    
     Serial.print("I received: ");
     Serial.println(incomingString);
   }
