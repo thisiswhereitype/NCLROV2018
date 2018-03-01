ddb096fd31466ee6b977d6975445663089bdd569
Antoine <antoinepetty13@gmail.com>
2018-03-01 14:27:47 +0000

    Working version for string UDP between surface and Pi

--- pi/udp_test.py
+++ pi/udp_test.py
@@ -1,25 +1,12 @@
 import socket
-import time
 
-UDP_IP = "169.254.89.200"
-UDP_PORT = 8888
-#MESSAGE = b'2000'
-MESSAGE = "Hello"
+UDP_IP = "169.254.116.33" #The Pi's IP
+UDP_PORT = 5005
 
-
-print ("UDP target IP:", UDP_IP)
-print ("UDP target port:", UDP_PORT)
-print ("message:", MESSAGE)
-
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-#sock.bind((UDP_IP, UDP_PORT))
-#sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-sock.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP, UDP_PORT))
-
-#sock.bind((UDP_IP, UDP_PORT))
+sock = socket.socket(socket.AF_INET, #internet
+                     socket.SOCK_DGRAM) #UDP
+sock.bind((UDP_IP, UDP_PORT))
 
 while True:
-    time.sleep(5)
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print ("received message:", data)
+    data, addr = sock.recvfrom(1024)
+    print ("received message:",data)
