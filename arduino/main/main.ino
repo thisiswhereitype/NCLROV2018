7e31b24b6287b611f89d8d9336dbb6858f8ac2d8
Antoine <36314396+antoinepetty@users.noreply.github.com>
2018-03-02 20:45:30 +0000

    Fixing sync issues (not finished)

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,49 +1,105 @@
 #include <WString.h>
 
 String incomingString = "";
-const int ARRAYSIZE = 9;
+const int ARRAYSIZE = 10;
 int inputArray[ARRAYSIZE];
+int arrayPointer=0;              // Int to point to current array position
+String inputString = "";         // a String to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
 
 void setup() {
   //Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
   Serial.begin(115200);     // opens serial port, sets data rate to 115200 bps
   Serial.setTimeout(1000);     //The default timeout is a second - way too slow for our purposes. Now set to 1ms
+  inputString.reserve(200); // reserve 200 bytes for the inputString
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
-
-  //Initialising the input array
-  if (Serial.available() > 0) {
-    //Not used
-    //incomingString = Serial.readString();
-  }
 }
 
 void loop() {
-  
-  // If receiving data:
-  if (Serial.available() > 0) {
-    for (int i=0; i<ARRAYSIZE; i++) {
-      incomingString = Serial.readString();
-      inputArray[i]=incomingString.toInt();
-  
-      // say what you got:
-      //delay(5); //Delay to allow some time for the arduino to actually read the data
-      
-      //Serial.print("I received: ");
-      Serial.print("[");
-      Serial.print(i);
-      Serial.print("]");
-      Serial.println(incomingString);
+  if (stringComplete) { //If new value received (ending in a newline character)
+
+    int currentValue = inputString.toInt();
+    
+    //currentValue = 11111;
+    if(currentValue == 11111 && arrayPointer != 0){
+      arrayPointer = 0; //If sync value is not in position 0, reset to position 0
+      Serial.print("Sync error. Now reset to position 0.");
     }
     
-    //if(incomingString.toInt()==1){
-    if(inputArray[8]==1){
+    inputArray[arrayPointer]= currentValue;
+
+    // say what you got:
+    
+    Serial.print("[");
+    Serial.print(arrayPointer);
+    Serial.print("]");
+    Serial.println(inputString);
+
+    if (arrayPointer==(ARRAYSIZE-1)){
+      arrayPointer = 0; //Reset arraypointer if it's reached the maximum array position
+    }
+    else{
+      arrayPointer = arrayPointer + 1; //Else increment arrayPointer
+    }
+
+    //Reset input string and wait for new input
+    inputString = "";
+    stringComplete = false;
+  }
+
+
+
+  if(inputArray[8]==1){
       digitalWrite(LED_BUILTIN, HIGH);
     }
     else{
       digitalWrite(LED_BUILTIN, LOW);
     }
-    
+  // If receiving data:
+//  if (Serial.available() > 0) {
+//
+//    for (int i=0; i<ARRAYSIZE; i++) {
+//      incomingString = Serial.readString();
+//      inputArray[i]=incomingString.toInt();
+//  
+//      // say what you got:
+//      //delay(5); //Delay to allow some time for the arduino to actually read the data
+//      
+//      //Serial.print("I received: ");
+//      Serial.print("[");
+//      Serial.print(i);
+//      Serial.print("]");
+//      Serial.println(incomingString);
+//    }
+//    
+//    //if(incomingString.toInt()==1){
+//    if(inputArray[8]==1){
+//      digitalWrite(LED_BUILTIN, HIGH);
+//    }
+//    else{
+//      digitalWrite(LED_BUILTIN, LOW);
+//    }
+//  }
+}
 
+
+
+/*
+  SerialEvent occurs whenever a new data comes in the hardware serial RX. This
+  routine is run between each time loop() runs, so using delay inside loop can
+  delay response. Multiple bytes of data may be available.
+*/
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
   }
 }
--- surface/surface_udp.py
+++ surface/surface_udp.py
@@ -3,20 +3,23 @@ import time
 
 #Define the array to be transferred
 ARRAYWIDTH = 2
-ARRAYHEIGHT = 9
+ARRAYHEIGHT = 10
 outputArray = [[0 for x in range(ARRAYWIDTH)] for y in range(ARRAYHEIGHT)]
 
 #Allocate slots for each output ( in format outputArray[thing][value] )
 #Thrusters have first 6 positions
-outputArray[0][0]="ForeTopThruster1"
-outputArray[1][0]="ForeTopThruster2"
-outputArray[2][0]="ForeLeftThruster"
-outputArray[3][0]="ForeRightThruster"
-outputArray[4][0]="AftLeftThruster"
-outputArray[5][0]="AftRightThruster"
-outputArray[6][0]="CameraX"
-outputArray[7][0]="CameraY"
-outputArray[8][0]="ForeLamp" #LED for illuminating surroundings
+outputArray[0][0]="Synchronisation" #Top value remains the same at all times just in case things become unsynchronised
+outputArray[1][0]="ForeTopThruster1"
+outputArray[2][0]="ForeTopThruster2"
+outputArray[3][0]="ForeLeftThruster"
+outputArray[4][0]="ForeRightThruster"
+outputArray[5][0]="AftLeftThruster"
+outputArray[6][0]="AftRightThruster"
+outputArray[7][0]="CameraX"
+outputArray[8][0]="CameraY"
+outputArray[9][0]="ForeLamp" #LED for illuminating surroundings
+
+outputArray[0][1]=11111 #Synchronisation value is 5 1s
 
 
 #Set up UDP
@@ -43,10 +46,10 @@ for i in range(0, ARRAYHEIGHT):
 
 
 while True:
-    time.sleep(0.1)
+    #time.sleep(0.1) #Give the pi some time to think
     print(count)
     count=count+1
-    outputArray[0][1]=count
+    outputArray[1][1]=count #Increment thruster 1 value for testing
     for i in range(0, ARRAYHEIGHT):
         currentValue=outputArray[i][1]
         sock.sendto(bytes(str(currentValue), "utf-8"), (UDP_IP, UDP_PORT))
@@ -54,7 +57,7 @@ while True:
 
     #Test to turn LED on and off
     time.sleep(1)
-    if(outputArray[8][1]==0):
-        outputArray[8][1] = 1
+    if(outputArray[9][1]==0):
+        outputArray[9][1] = 1
     else:
-        outputArray[8][1] = 0
+        outputArray[9][1] = 0
