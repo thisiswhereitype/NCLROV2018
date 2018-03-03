f864e5904f92cf4ffcb1468c6415d9d824f2c562
Antoine <antoinepetty13@gmail.com>
2018-03-03 20:58:18 +0000

    Fully working Surface-ROV Communication

--- pi/main.py
+++ pi/main.py
@@ -88,8 +88,8 @@ def arduino_comm_a(thread_name):
         i = 0
         while i < INPUT_ARRAY_HEIGHT:
             # get current value
-            input_array[i][1]=(ser.readline().decode("utf-8"))
-            if ((input_array[i][1] == "11111" and i != 0)or (input_array[i][1] != "11111" and i == 0)):
+            input_array[i][1]=(ser.readline().decode("utf-8")).rstrip()
+            if ((int(input_array[i][1]) == 11111 and i != 0)or (int(input_array[i][1]) != 11111 and i == 0)):
                 # If value 11111 found anywhere other than position 0, or position 0 is not 11111, then reset to position 0
                 # This is to avoid writing incorrect values if there are sync issues which would cause erratic behaviour of the ROV
                 print("Data sync error from arduino at position",i,". Current position reset to 0.")
